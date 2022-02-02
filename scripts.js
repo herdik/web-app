92b095a7eedcc1513cb90aa7eb533b49ad310415
herdik <juraj.herda1983@gmail.com>
2022-02-02 23:20:46 +0100

    thumbnails active

--- scripts.js
+++ scripts.js
@@ -1,18 +1,43 @@
-let data = {
+let data0 = {
     photo: 'lake.jpg',
     title: 'My title',
-    description: 'What happened here, why is this a very nice image'
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   };
   
+  let data1 = {
+    photo: 'mountain.jpg',
+    title: 'My Hora',
+    description: 'Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  };
+  
+  let data2 = {
+    photo: 'sky.jpg',
+    title: 'Sky',
+    description: 'Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  };  
+
+  let data3 = {
+    photo: 'tree.jpg',
+    title: 'Tree',
+    description: 'Incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  };  
+
+let currentPhoto = 0;
+let imagesData = [ data0, data1, data2, data3];
 
   
 
-let currentPhoto = 0;
-let imagesData = ['lake.jpg', 'mountain.jpg', 'sky.jpg', 'tree.jpg'];
+
 $('#photo').attr('src', imagesData[currentPhoto]);
 
 let loadPhoto = (index) => {
-    $('#photo').attr('src', imagesData[index]);}
+    $('#photo').attr('src', imagesData[index].photo);
+    $('#title').text(imagesData[index].title);
+    $('#description').text(imagesData[index].description);
+}
+
+loadPhoto(currentPhoto); /*can be '0'*/
+
 
 $('.rightarrow').on('click', () => { 
     console.log('clicked'); 
@@ -33,3 +58,16 @@ $('.leftarrow').on('click', () => {
     }
     loadPhoto(currentPhoto);
 }); 
+
+let i = 0;
+imagesData.forEach(arrayObject => {
+    $('#thumbnails').append(`<img src="${arrayObject.photo}"  dataIndex="${i}"></img>`);
+    i++;
+  });
+
+ 
+
+  $('#thumbnails img').click((event) => {
+	currentPhoto = $(event.target).attr('dataIndex');
+	loadPhoto(currentPhoto);
+});
